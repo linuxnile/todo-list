acf48f6547ccc8feff014834d8c0dae3a8c02bd0
linux nile <linuxnile@gmail.com>
2023-12-13 10:18:32 +0530

    Done
    Watch youtube video for more understanding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Todo from "./components/todoreact/todo";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Todo />
+    </>
+  );
+};
 
-export default App
+export default App;
